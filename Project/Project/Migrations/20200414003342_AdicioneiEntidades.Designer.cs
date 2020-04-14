1723699288d0a46a5d59011b625733db02feb039
jeferson Augusto <carvalho.jeferson@gmail.com>
2020-04-13 21:37:36 -0300

    Adicionada nova migration

--- Project/Project/Migrations/20200414003342_AdicioneiEntidades.Designer.cs
+++ Project/Project/Migrations/20200414003342_AdicioneiEntidades.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Project.Data;
 
 namespace Project.Migrations
 {
     [DbContext(typeof(ProjectContext))]
-    partial class ProjectContextModelSnapshot : ModelSnapshot
+    [Migration("20200414003342_AdicioneiEntidades")]
+    partial class AdicioneiEntidades
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
